57bff1b1026e2c118b12b34065e79f02aff7e4c9
Palafox <jdvzzpl@gmail.com>
2023-01-30 16:56:32 -0600

    Update

--- romberg.f90
+++ romberg.f90
@@ -1,7 +1,7 @@
 program main
    implicit none
    real::   romberg
-   write(*,*) romberg(1.0,3.0,3,3)
+   write(*,*) romberg(1.0,3.0,3,2)
 end program main
 
 real function f(x)
@@ -12,27 +12,33 @@ real function f(x)
 end function f
 
 real function romberg(a, b, i, j)
+!Esta función realiza integración numérica por medio del método de Romberg(extrapolación de Richardson)
+!Inputs:
+!   a: limite inferior de integración
+!   b: limite superior de integración
+!   i: 1,2,3, ...
+!   j: 1,2,3, ...
+!Output:
+!   r: valor de la integral 
    implicit none
    real  :: f, a, b, sum, h
    integer  :: i, j, k, l, m
    real  :: r(i,j)
 
-   
-   sum=0
+   sum=0 !se almacena el valor de la sumatoria
    do k=1,i
       do l=1,k
-         if ( k==1 .and. l==1 ) then
+         if ( k==1 .and. l==1 ) then   !calcula r11
             h = (b-a)/k
             r(1,1) = h/2 *( f(a) + f(b) )
-
-         else if ( k>=2 .and. l==1 ) then
-            do m=1, 2**(i-2)
+         else if ( k>=2 .and. l==1 ) then !calcula ri1
+            do m=1, 2**(k-2)
                sum = sum + f(a + (2*m-1)*h/2 )
             end do
             r(k,1) =  0.5 * ( r(k-1,1) + h*sum )
             h = (b-a)/k
             sum=0
-         else
+         else  !calcula rij, i>=2 j>2
             r(k,l) = ( 4**(l-1)*r(k,l-1)-r(k-1,l-1) )/( 4**(l-1)-1 )
          end if
 
--- romberg.py
+++ romberg.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 def f(x):
+   'Función que se desea integrar'
    f=1/x
    return f
 
-def romberg(f, a, b, k, l):
+def romberg(a, b, k, l):
    '''
    Esta función realiza integración numérica por medio del método de Romberg(extrapolación de Richardson)
    Inputs:
-      f: función que se desea integrar
       a: limite inferior de integración
       b: limite superior de integración
       k: 1,2,3, ...
@@ -19,12 +19,14 @@ def romberg(f, a, b, k, l):
    if l>k:
       print('Error, j es mayor a i')
       return 
+   if l==0 or k==0:
+      print('Error')
+      return
 
    r=np.zeros((k,l)) #array donde se guardan los valores de rij
    sum=0
    for i in range(1,k+1):
       for j in range(1,i+1):
-
          if ( i==1 and j==1 ):   #Calcula R11
             h = (b-a)/i
             r[0,0] = h/2 *( f(a) + f(b) )
@@ -34,9 +36,8 @@ def romberg(f, a, b, k, l):
             r[i-1,0] = 0.5 * ( r[i-2,0] + h*sum )
             h = (b-a)/i
             sum=0
-         else: #Calcula Rij
+         else: #Calcula Rij, i>=2 j>2
             r[i-1,j-1] = ( 4**(j-1)*r[i-1,j-2]-r[i-2,j-2] )/( 4**(j-1)-1 )
    return r[k-1,l-1]
 
-print(romberg(f,1,3,3,3))
-
+print(romberg(1,3,3,3))
