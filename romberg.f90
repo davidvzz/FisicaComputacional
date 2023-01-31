1d3385dfd2532c593b21031fec5be49755a80a28
Palafox <jdvzzpl@gmail.com>
2023-01-31 11:31:36 -0600

    Update

--- romberg.f90
+++ romberg.f90
@@ -1,7 +1,7 @@
 program main
    implicit none
    real::   romberg
-   write(*,*) romberg(1.0,3.0,3,2)
+   write(*,*) romberg(1.0,3.0,3,3)
 end program main
 
 real function f(x)
@@ -31,6 +31,7 @@ real function romberg(a, b, i, j)
          if ( k==1 .and. l==1 ) then   !calcula r11
             h = (b-a)/k
             r(1,1) = h/2 *( f(a) + f(b) )
+
          else if ( k>=2 .and. l==1 ) then !calcula ri1
             do m=1, 2**(k-2)
                sum = sum + f(a + (2*m-1)*h/2 )
@@ -38,10 +39,11 @@ real function romberg(a, b, i, j)
             r(k,1) =  0.5 * ( r(k-1,1) + h*sum )
             h = (b-a)/k
             sum=0
+
          else  !calcula rij, i>=2 j>2
             r(k,l) = ( 4**(l-1)*r(k,l-1)-r(k-1,l-1) )/( 4**(l-1)-1 )
          end if
-
+         
       end do
    end do
    romberg=r(i,j)
--- romberg.py
+++ romberg.py
@@ -26,18 +26,21 @@ def romberg(a, b, k, l):
    r=np.zeros((k,l)) #array donde se guardan los valores de rij
    sum=0
    for i in range(1,k+1):
-      for j in range(1,i+1):
+      for j in range(1,l+1):
          if ( i==1 and j==1 ):   #Calcula R11
             h = (b-a)/i
             r[0,0] = h/2 *( f(a) + f(b) )
+
          elif ( i>=2 and j==1 ):   #Calcula Ri1
             for m in range(1,2**(i-2)+1): 
                sum=sum + f(a + (2*m-1)*h/2 ) #sumatoria 
             r[i-1,0] = 0.5 * ( r[i-2,0] + h*sum )
             h = (b-a)/i
             sum=0
+
          else: #Calcula Rij, i>=2 j>2
             r[i-1,j-1] = ( 4**(j-1)*r[i-1,j-2]-r[i-2,j-2] )/( 4**(j-1)-1 )
+            
    return r[k-1,l-1]
 
 print(romberg(1,3,3,3))
